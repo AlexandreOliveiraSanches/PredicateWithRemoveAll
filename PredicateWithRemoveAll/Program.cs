df94a148130a21221f9e568737ab910c3052b6de
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-09-16 13:09:49 -0300

    Utilizando Predicate para remover produtos de uma lista

---
+++ PredicateWithRemoveAll/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PredicateWithRemoveAll.Entities;
+using System.Collections.Generic;
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> p = new List<Product>();
+            p.Add(new Product("Tv", 950));
+            p.Add(new Product("Mouse", 80.50));
+            p.Add(new Product("Tablet", 380));
+            p.Add(new Product("Teclado", 92.70));
+
+            p.RemoveAll(ProductTest);
+
+            foreach(Product product in p)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100;
+        }
+    }
+}
